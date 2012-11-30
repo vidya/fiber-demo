50c1d3507e284efdfcc7aece0347a8bee7f33fb1
vidya <vsagarj@gmail.com>
2012-11-29 19:58:30 -0800

    Refactor
    fiber_dictionary_search.rb
     - refactor

--- fiber_dictionary_search.rb
+++ fiber_dictionary_search.rb
@@ -2,36 +2,31 @@ require 'pry'
 require 'fiber'
 
 class FiberDictionarySearch
-  attr_accessor :fiber_list, :word_pairs
+  attr_accessor :word_pairs
 
   def initialize(filename)
-    puts "=== tr8 ==========="
+    puts "=== crane ==========="
 
-    @fiber_list = {
-        :read_seg             => create_read_segments_fiber(filename),
-        :delete_short_words   => create_delete_short_words_fiber,
-        :tail_swap_pairs      => create_tail_swap_pairs_fiber
-    }
-
-    @word_pairs = compute_word_pairs
+    @word_pairs = compute_word_pairs(filename)
   end
 
   #---------------------------------------------------------------------------------------
   private
-  def compute_word_pairs
+  def compute_word_pairs(filename)
+    read_seg_fiber             = create_read_segments_fiber(filename)
+    delete_short_words_fiber   = create_delete_short_words_fiber
+    tail_swap_pairs_fiber      = create_tail_swap_pairs_fiber
+    
     tail_swap_pairs_list = []
-
-    read_seg_fiber              = fiber_list[:read_seg]
-    delete_short_words_fiber    = fiber_list[:delete_short_words]
-    tail_swap_pairs_fiber       = fiber_list[:tail_swap_pairs]
 
     while read_seg_fiber.alive?
       dict_seg            = read_seg_fiber.resume
       dict_seg            = delete_short_words_fiber.resume dict_seg
 
-      tail_swap_pairs     = tail_swap_pairs_fiber.resume dict_seg
+      #tail_swap_pairs     = tail_swap_pairs_fiber.resume dict_seg
 
-      tail_swap_pairs.each { |sw_pair| tail_swap_pairs_list << sw_pair }
+      #tail_swap_pairs.each { |sw_pair| tail_swap_pairs_list << sw_pair }
+      tail_swap_pairs_fiber.resume(dict_seg).each { |sw_pair| tail_swap_pairs_list << sw_pair }
     end
 
     tail_swap_pairs_list
@@ -39,12 +34,11 @@ class FiberDictionarySearch
 
   #--- fiber: read_segments
   def create_read_segments_fiber(filename)
-    dict        = File.readlines(filename).map { |ln| ln.chomp }
-    let_list    = ('a'..'z').to_a
-
     Fiber.new do
-      let_list.each do |let|
-        puts "create_read_segments_fiber: #{let}"
+      dict = File.readlines(filename).map { |ln| ln.chomp }
+
+      ('a'..'z').each do |let|
+        puts "let: #{let}"
 
         let_seg = dict.select { |word| word.start_with? let }
 
