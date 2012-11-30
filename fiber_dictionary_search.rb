5734db44936d892f70ba8c5030d678a78eebaa6f
vidya <vsagarj@gmail.com>
2012-11-29 16:52:54 -0800

    Comment outdated code
    fiber_dictionary_search.rb
     - comment outdated code

--- fiber_dictionary_search.rb
+++ fiber_dictionary_search.rb
@@ -5,9 +5,9 @@ require 'fiber'
 require './lib_dictionary_search'
 
 class FiberDictionarySearch
-  include LibDictionarySearch
+  #include LibDictionarySearch
 
-  attr_accessor :dict, :fiber_list, :letter_segment, :alphabet_list, :reversible_suffix_words
+  attr_accessor :dict, :fiber_list, :reversible_suffix_words
 
   def initialize(filename)
     @dict = File.readlines(filename).map { |ln| ln.chomp }
@@ -29,47 +29,15 @@ class FiberDictionarySearch
     while read_seg_fiber.alive?
       dict_seg      = read_seg_fiber.resume
       dict_seg      = delete_short_words_fiber.resume dict_seg
-      #binding.pry
 
       swap_pairs    = create_tail_swap_pairs_fiber.resume dict_seg
       swap_pairs.each { |sw_pair| swap_pairs_list << sw_pair }
-
-      #binding.pry
     end
 
     binding.pry
     swap_pairs_list
-    #[first_seg, second_seg, third_seg]
   end
 
-
-  #def run
-  #  read_seg_fiber              = fiber_list[:read_seg]
-  #  delete_short_words_fiber    = fiber_list[:delete_short_words]
-  #  tail_swap_pairs_fiber       = fiber_list[:tail_swap_pairs]
-  #
-  #  first_seg   = read_seg_fiber.resume
-  #  first_seg   = delete_short_words_fiber.resume first_seg
-  #  #binding.pry
-  #  first_seg_swap_pairs   = create_tail_swap_pairs_fiber.resume first_seg
-  #  #binding.pry
-  #  puts 'after first_seg'
-  #
-  #  second_seg  = read_seg_fiber.resume
-  #  second_seg  = delete_short_words_fiber.resume second_seg
-  #  binding.pry
-  #  second_seg_swap_pairs   = create_tail_swap_pairs_fiber.resume second_seg
-  #  binding.pry
-  #  puts 'after second_seg'
-  #
-  #  third_seg   = read_seg_fiber.resume
-  #  third_seg   = delete_short_words_fiber.resume third_seg
-  #  puts 'after second_seg'
-  #
-  #  [first_seg, second_seg, third_seg]
-  #end
-  #
-  #
 
   #--- fiber: read_segments
   def create_read_segments_fiber(filename)
@@ -123,73 +91,73 @@ class FiberDictionarySearch
 end
 
 
-class OldFiberDictionarySearch
-  include LibDictionarySearch
-
-  attr_accessor :dict, :letter_segment, :alphabet_list, :reversible_suffix_words
-
-  def initialize(file_path)
-    @dict             = read_data file_path
-
-    @alphabet_list    = ('a'..'z').to_a
-
-    @letter_segment   = get_letter_segments
-
-    @reversible_suffix_words = []
-  end
-
-  def word_pairs
-    if reversible_suffix_words.empty?
-      fw1 = create_fiber
-      fw2 = create_fiber
-      fw3 = create_fiber
-
-      list1 = ('a'..'h').to_a
-      list2 = ('i'..'r').to_a
-      list3 = ('s'..'z').to_a
-
-      while true
-        list1, rv1 = fw1.resume(list1) if fw1.alive?
-        list2, rv2 = fw2.resume(list2) if fw2.alive?
-        list3, rv3 = fw3.resume(list3) if fw3.alive?
-
-        append_rv_list rv1 unless rv1.nil?
-        append_rv_list rv2 unless rv2.nil?
-        append_rv_list rv3 unless rv3.nil?
-
-        break unless (rv1 || rv2 || rv3)
-      end
-    end
-
-    reversible_suffix_words
-  end
-
-  #--- private ------------------------------------------------------------------------------
-  private
-  def read_data(filename)
-    File.readlines(filename).map { |ln| ln.chomp }
-  end
-
-  def get_letter_segments
-    alphabet_list.inject({}) do |let_seg_hash, let|
-      let_seg_hash[let] = dict.select { |word| word.start_with? let }
-
-      let_seg_hash
-    end
-  end
-
-  def append_rv_list(rv_list)
-    rv_list.each { |rv| reversible_suffix_words << rv }
-  end
-
-  def create_fiber
-    Fiber.new do |list|
-      until list.empty?
-        let = list.pop
-        puts "let = #{let}"
-
-        Fiber.yield list[0..-1], select_reversible_suffix_words(delete_short_words @letter_segment[let])
-      end
-    end
-  end
-end
+#class OldFiberDictionarySearch
+#  include LibDictionarySearch
+#
+#  attr_accessor :dict, :letter_segment, :alphabet_list, :reversible_suffix_words
+#
+#  def initialize(file_path)
+#    @dict             = read_data file_path
+#
+#    @alphabet_list    = ('a'..'z').to_a
+#
+#    @letter_segment   = get_letter_segments
+#
+#    @reversible_suffix_words = []
+#  end
+#
+#  def word_pairs
+#    if reversible_suffix_words.empty?
+#      fw1 = create_fiber
+#      fw2 = create_fiber
+#      fw3 = create_fiber
+#
+#      list1 = ('a'..'h').to_a
+#      list2 = ('i'..'r').to_a
+#      list3 = ('s'..'z').to_a
+#
+#      while true
+#        list1, rv1 = fw1.resume(list1) if fw1.alive?
+#        list2, rv2 = fw2.resume(list2) if fw2.alive?
+#        list3, rv3 = fw3.resume(list3) if fw3.alive?
+#
+#        append_rv_list rv1 unless rv1.nil?
+#        append_rv_list rv2 unless rv2.nil?
+#        append_rv_list rv3 unless rv3.nil?
+#
+#        break unless (rv1 || rv2 || rv3)
+#      end
+#    end
+#
+#    reversible_suffix_words
+#  end
+#
+#  #--- private ------------------------------------------------------------------------------
+#  private
+#  def read_data(filename)
+#    File.readlines(filename).map { |ln| ln.chomp }
+#  end
+#
+#  def get_letter_segments
+#    alphabet_list.inject({}) do |let_seg_hash, let|
+#      let_seg_hash[let] = dict.select { |word| word.start_with? let }
+#
+#      let_seg_hash
+#    end
+#  end
+#
+#  def append_rv_list(rv_list)
+#    rv_list.each { |rv| reversible_suffix_words << rv }
+#  end
+#
+#  def create_fiber
+#    Fiber.new do |list|
+#      until list.empty?
+#        let = list.pop
+#        puts "let = #{let}"
+#
+#        Fiber.yield list[0..-1], select_reversible_suffix_words(delete_short_words @letter_segment[let])
+#      end
+#    end
+#  end
+#end
